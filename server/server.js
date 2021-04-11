52bf2b21e67c278d195f23a2f1dcde9ece54e45c
Elham Ahmadzadeh <eliahm63@gmail.com>
2021-04-11 21:51:43 +0200

    server has edited

--- server/server.js
+++ server/server.js
@@ -15,7 +15,9 @@ app.use(express.json())
 app.get('/api/v1/restaurants', async (req, res) => {
   try {
     const restaurantRatingsResult = await db.query(
-      'SELECT * FROM restaurants  LEFT JOIN (SELECT restaurant_id,  COUNT(*), TRUNC(AVG(rating), 2) AS Average_Rating FROM reviews GROUP BY restaurant_id)reviews ON restaurants.id = reviews.restaurant_id;'
+      `SELECT * FROM restaurants  LEFT JOIN (SELECT restaurant_id,
+        COUNT(*), TRUNC(AVG(rating), 2) AS Average_Rating FROM reviews GROUP BY restaurant_id)
+        reviews ON restaurants.id = reviews.restaurant_id;`
     )
 
     res.status(200).json({
@@ -35,7 +37,9 @@ app.get('/api/v1/restaurants/:id', async (req, res) => {
   console.log(req.params.id)
   try {
     const restaurant = await db.query(
-      'SELECT * FROM restaurants  LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating), 2) AS Average_Rating FROM reviews GROUP BY restaurant_id)reviews ON restaurants.id = reviews.restaurant_id WHERE id = $1',
+      `SELECT * FROM restaurants  LEFT JOIN (SELECT restaurant_id, COUNT(*),
+      TRUNC(AVG(rating), 2) AS Average_Rating FROM reviews GROUP BY restaurant_id)
+      reviews ON restaurants.id = reviews.restaurant_id WHERE id = $1`,
       [req.params.id]
     )
 
@@ -59,7 +63,8 @@ app.get('/api/v1/restaurants/:id', async (req, res) => {
 app.post('/api/v1/restaurants/:id/addReview', async (req, res) => {
   try {
     const newReview = await db.query(
-      'INSERT INTO reviews (restaurant_id, name, review, rating) values ($1, $2, $3, $4) returning *',
+      `INSERT INTO reviews (restaurant_id, name, review, rating)
+                    values ($1, $2, $3, $4) returning *`,
       [req.params.id, req.body.name, req.body.review, req.body.rating]
     )
     console.log(newReview)
@@ -79,7 +84,8 @@ app.post('/api/v1/restaurants', async (req, res) => {
   console.log(req.body)
   try {
     const result = await db.query(
-      'INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *',
+      `INSERT INTO restaurants (name, location, price_range)
+                        values ($1, $2, $3) returning *`,
       [req.body.name, req.body.location, req.body.price_range]
     )
     console.log(result)
@@ -100,7 +106,8 @@ app.put('/api/v1/restaurants/:id', async (req, res) => {
   //console.log(req.body)
   try {
     const result = await db.query(
-      'UPDATE restaurants SET name = $1 ,location = $2 ,price_range = $3 WHERE id = $4 RETURNING *',
+      `UPDATE restaurants SET name = $1 ,location = $2 ,price_range
+              = $3 WHERE id = $4 RETURNING *`,
       [req.body.name, req.body.location, req.body.price_range, req.params.id]
     )
     console.log(result)
